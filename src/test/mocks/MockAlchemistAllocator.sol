e660eda8e21f4294d399f068a13e384266a56c02
silur <3187667+Silur@users.noreply.github.com>
2025-11-24 12:37:56 +0400

    decouple

--- src/test/mocks/MockAlchemistAllocator.sol
+++ src/test/mocks/MockAlchemistAllocator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {AlchemistAllocator} from "../../AlchemistAllocator.sol";
+import {AlchemistAllocator} from "../../../lib/alchemix-ac/src/AlchemistAllocator.sol";
 
 contract MockAlchemistAllocator is AlchemistAllocator {
     constructor(address _myt, address _admin, address _operator) AlchemistAllocator(_myt, _admin, _operator) {}
--- src/test/mocks/MockAlchemistCurator.sol
+++ src/test/mocks/MockAlchemistCurator.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-import {AlchemistCurator} from "../../AlchemistCurator.sol";
+import {AlchemistCurator} from "../../../lib/alchemix-ac/src/AlchemistCurator.sol";
 
 contract MockAlchemistCurator is AlchemistCurator {
     constructor(address _admin, address _operator) AlchemistCurator(_admin, _operator) {}
